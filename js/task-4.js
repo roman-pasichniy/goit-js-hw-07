ecf91a8666e06002296dba43515b1f5e304f97b3
Roman Pasichniy <roman.pasichniy@gmail.com>
2026-02-21 23:46:27 +0200

    all tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    
+    const login = form.elements.email.value;
+    const password = form.elements.password.value;
+
+    if (!login || !password) {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    const userInfo = {};
+    userInfo[form.elements.email.name] = login.trim();
+    userInfo[form.elements.password.name] = password.trim();
+
+    console.log(userInfo);
+
+    form.reset();
+});
+
+
+
+
+
